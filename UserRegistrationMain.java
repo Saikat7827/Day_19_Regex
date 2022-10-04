10ac8f47627690645cb825b53dcb4991b44c3ed3
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-10-04 16:06:49 +0530

    Valid first name

---
+++ UserRegistrationMain.java
@@ -0,0 +1,35 @@
+package com.bl.Regex;
+
+import java.util.Scanner;
+
+public class UserRegistrationMain {
+	static Scanner scanner = new Scanner(System.in);
+
+	
+	public static void main(String[] args) {
+		while (true) {
+
+			System.out.println("Enter 0 : Exit");
+			System.out.println("Enter 1 : To validate First Name ");
+			System.out.println("Enter 2 : To validate Last Name ");
+			System.out.println("Enter 3 : To validate E-mail ");
+
+			switch (scanner.nextInt()) {
+			case 0:
+				System.exit(0);
+
+			case 1:
+				UserRegistration.validFirstName();
+				break;
+
+			case 2:
+				UserRegistration.validFirstName();
+				break;
+
+			case 3:
+				UserRegistration.validEmail();
+				break;
+			}
+		}
+	}
+}
